e54956da357a38c698e2e529f86d383bd210e125
artyuhvladislav <workto22@gmail.com>
2019-10-04 13:06:08 +0300

    feat: add calc task

--- src/index.js
+++ src/index.js
@@ -2,11 +2,40 @@ function eval() {
     // Do not use eval!!!
     return;
 }
+let divide = function(expr) {
+	let numberString = expr.split('/');
+	let numbers =numberString.map(noStr=> +noStr); 
+	let result = numbers.reduce((sum, current) => (sum/current));
+	return result;
+}
+let multi = function(expr) {
+	let numberString = expr.split('*');
+	let numbers =numberString.map((noStr)=> divide(noStr));
+	let initialValue = 1.0000;
+	let result = numbers.reduce((sum, current) => sum * current, initialValue);
+	return result;
+} 
 
-function expressionCalculator(expr) {
-    // write your solution here
+let minus = function(expr) {
+	let  numberString = expr.split('-');
+	let numbers = numberString.map(noStr =>multi(noStr));
+	let initialValue = numbers[0];
+	let result = numbers.slice(1).reduce((sum, current) => sum - current, initialValue);
+	return result;
+}
+let expressionCalculator = function(expr) {
+	let  numberString = expr.split('+');
+	let numbers = numberString.map(noStr => minus(noStr));
+	let initialValue = 0.0000;
+	let result = numbers.reduce((sum, current) => sum + current , initialValue);
+
+	return result;
 }
 
 module.exports = {
-    expressionCalculator
+	expressionCalculator
 }
+
+
+
+
